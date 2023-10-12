9e6e688fcc347267cd4c292c32f4e367cb320fa9
jenifery <jenifers@revature.com>
2023-10-12 02:07:12 -0400

    first

---
+++ SQLQuery1.sql
@@ -0,0 +1,127 @@
+
+
+use inter
+
+-- Concurrency problem
+
+
+create table customer(custid int primary key,cuscode varchar(10),cusname varchar(20))
+
+insert into customer(custid,cuscode,cusname)
+values
+(1,'code_1','Amy'),
+(2,'code_2','Aron'),
+(3,'code_3','Steve'),
+(4,'code_4','Julie')
+
+select * from customer
+
+-- transaction
+
+begin transaction 
+
+update customer set cuscode='code_101'
+where custid=1
+
+waitfor delay '00:00:10'
+
+update customer set cuscode='code_1011'
+where custid=1
+
+rollback transaction
+
+-- dirty read
+
+begin transaction
+
+update customer set cusname='Amy Jackson'
+where custid =1
+
+-- amy wants to add the surname to billing
+
+waitfor delay '00:00:15'
+
+-- changes her mind and decided to go ahead with the old details
+
+rollback transaction
+
+
+-- create table product
+
+create table pro(id int primary key,name varchar(100),quantity int)
+
+insert into pro(id,name,quantity)
+values
+(1001,'mobile',10),
+(1002,'Tablet',20),
+(1003,'laptop',30)
+
+select * from pro
+
+-- lost update 
+
+-- transaction 1
+
+set transaction isolation level repeatable read
+
+begin transaction
+
+declare @quantity int
+
+select  @quantity=quantity from pro where id=1001
+
+waitfor delay '00:00:10'
+
+set @quantity=@quantity-1
+update pro set quantity=@quantity where id =1001
+print @quantity
+
+commit transaction
+
+
+-- Non repeatable read problem
+
+--set transaction isolation level read committed
+
+set transaction isolation level repeatable read
+
+begin transaction
+
+select quantity from pro where id=1001
+
+--wait time
+
+waitfor delay '00:00:15'
+
+select quantity from pro where id=1001
+
+commit transaction
+
+-- Phantom read problem
+-- Transaction 1 
+
+set transaction isolation level serializable
+begin transaction
+select count(*) from pro
+waitfor delay '00:00:15'
+select count(*) from pro
+commit transaction
+
+
+-- snapshot 
+
+--set transaction isolation level serializable
+
+begin transaction
+select count(*) from pro
+waitfor delay '00:00:15'
+select count(*) from pro
+commit transaction
+
+
+
+
+
+
+
+
---
+++ SQLQuery2.sql
@@ -0,0 +1,62 @@
+
+
+use inter
+
+
+set transaction isolation level read committed
+select * from customer where custid=1
+
+-- transaction 2
+
+select * from pro
+
+
+set transaction isolation level repeatable read
+
+begin transaction
+
+declare @quantity int
+
+select  @quantity=quantity from pro where id=1001
+
+--waitfor delay '00:00:10'
+
+set @quantity=@quantity-2
+
+update pro set quantity=@quantity where id =1001
+print @quantity
+
+commit transaction
+
+
+-- transaction 2
+
+--set transaction isolation level read committed
+
+set transaction isolation level repeatable read
+update pro set quantity=3 where id=1001
+
+
+-- transaction 2
+
+
+set transaction isolation level serializable
+begin transaction
+insert into pro values(1010,'pods',12)
+select count(*) from pro
+commit transaction
+
+-- set transaction isolation level serializable
+
+alter database inter
+set allow_snapshot_isolation on 
+begin transaction
+insert into pro values(1004,'pods',12)
+select count(*) from pro
+commit transaction
+
+
+
+delete from pro where id between 1004 and 1111
+
+select * from pro
