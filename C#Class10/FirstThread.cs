88e0f5025214387d9677069f137071403a16412e
jenifery <jenifers@revature.com>
2023-10-31 03:54:54 -0400

    second

---
+++ C#Class10/FirstThread.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Class10
+{
+    internal class FirstThread
+    {
+        public static void work1()
+        {
+            Console.WriteLine("Thread 1");
+        }
+
+        public static void child1()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("this is child Thread 1");
+                // Thread.Sleep(500);
+            }
+
+        }
+        public static void child2()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("this is child Thread 2");
+                //Thread.Sleep(500);
+            }
+        }
+
+        public static void BackgroundThread()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Background thread is in progress" + i);
+                Thread.Sleep(500);
+            }
+            Console.WriteLine("Background thread quits");
+        }
+    }
+}
--- C#Class10/Program.cs
+++ C#Class10/Program.cs
@@ -6,17 +6,51 @@ class Program
 {
     public static void Main()
     {
-        ScrapMethod sc = new ScrapMethod();
+
+        //Thread th = Thread.CurrentThread;
+        //th.Name = "Main thread";
+        //Console.WriteLine(th.Name);
+        Console.WriteLine("Main thread starts");
+
+        Thread bgthread = new Thread(FirstThread.BackgroundThread)
+        {
+            IsBackground = false
+        };
+        bgthread.Start();
+
+        Console.WriteLine("Main thread quits");
+
+        //Thread t2 = new Thread(FirstThread.child1)
+        //{
+        //    IsBackground = true
+        //};
+
+        //t2.Start();
+
+
+
+        //  Thread t1 = new Thread(FirstThread.work1);
+        //  t1.Start();
+
+
+
+
+
+        //t3.Start();
+        //t3.Join();
+
+
+        //ScrapMethod sc = new ScrapMethod();
         //sc.oldMethod();
-        sc.newMethod();
+        //sc.newMethod();
         //Student st = new Student();
         //st.showJsonData();
-       // Movie mv = new Movie();
+        // Movie mv = new Movie();
         //mv.XMLSerialization();
-      //  mv.XMLDeSerialization();
+        //  mv.XMLDeSerialization();
 
-       // PropertyCare prop1 = new PropertyCare("Fera", 121212, "Chennai");
-       // prop1.BinarySerializer();
-       // prop1.BinaryDeserializer();
+        // PropertyCare prop1 = new PropertyCare("Fera", 121212, "Chennai");
+        // prop1.BinarySerializer();
+        // prop1.BinaryDeserializer();
     }
 }
