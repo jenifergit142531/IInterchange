de8459f7dd7c0fd3ec24e1f336d710f6a90777cd
jenifergit142531 <76467762+jenifergit142531@users.noreply.github.com>
2023-11-02 03:43:12 -0400

    Update Login.cs

--- c#class11/Login.cs
+++ c#class11/Login.cs
@@ -1,5 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -10,18 +13,43 @@ namespace c_class11
     internal class Login : ILogin
     {
 
-        public string uname;
+        public string username;
+
+        [DataType(DataType.Password)]
         public string pwd;
-        static SqlCommand cmd, cmd2;
+        static SqlCommand cmd;
 
         public static string connectionString = "Data Source=REV-PG02C4Y5;Initial Catalog=property;Integrated Security=True;Persist Security Info=False;";
 
-        public void check()
+        public void LoginUserInput()
         {
             Console.WriteLine("Enter username");
-            uname = Console.ReadLine();
+            username = Console.ReadLine();
             Console.WriteLine("Enter password");
             pwd = Console.ReadLine();
+        }
+
+        public void UserRegistration()
+        {
+            LoginUserInput();
+            using (var con = new SqlConnection(connectionString))
+            {
+
+                
+                using (var cmd = new SqlCommand("userregistration", con))
+                {
+
+                    
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@pwd", pwd);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("User registration successfull");
+                    logincheck();
+
+                }
+            }
         }
 
 
@@ -30,31 +58,49 @@ namespace c_class11
             using (var con = new SqlConnection(connectionString))
             {
 
+                LoginUserInput();
 
-                con.Open();
-
-                using (var cmd = new SqlCommand("select count(*) from login where username = @uname AND pwd=@pwd", con))
+                using (var cmd = new SqlCommand("select dbo.loginchecking(@username,@pwd)", con))
                 {
-
-                    cmd.Parameters.AddWithValue("@username", uname);
+                    
+                    
+                    cmd.Parameters.AddWithValue("@username", username);
                     cmd.Parameters.AddWithValue("@pwd", pwd);
+                    con.Open();
                     int count = Convert.ToInt32(cmd.ExecuteScalar());
+                        
 
                     if (count == 1)
                     {
 
-                        Console.WriteLine("Existing user");
+                        Console.WriteLine("welcome "+ username);
+                        Program p = new Program();
+                        p.Menu();
+                        Console.WriteLine("Would you like to continue (Y / N)");
+                        char choice = Convert.ToChar(Console.ReadLine());
+                        while (choice == 'Y' && choice != 'N')
+                        {
+                            p.Menu();
+                        }
                     }
                     else
                     {
-                        Console.WriteLine("Invalid user");
-                        //logincheck();
+                        Console.WriteLine("User profile not available,Register Yourself");
+                        Console.WriteLine("Choose \n 1.Register Yourself \n 2.Exit");
+                        int options = Convert.ToInt32(Console.ReadLine());
+                        switch(options)
+                        {
+                            case 1:
+                                UserRegistration();
+                                break;
 
-                        //cmd2 = new SqlCommand("insert into login values(@uname,@pwd)", con);
-                        //{                       
-                        //    cmd2.ExecuteNonQuery();
-                        //    Console.WriteLine("User is successfully registered");
-                        //}
+                            case 2:
+                                Environment.Exit(0);
+                                break;
+                            default:
+                                break;
+                        }
+                        
                     }
                 }
             }
