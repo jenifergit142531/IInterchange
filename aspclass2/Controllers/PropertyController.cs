a891ba10f920bee2c7b68791851a9949636bb2cc
jenifery <jenifers@revature.com>
2023-11-07 00:11:36 -0500

    first

---
+++ aspclass2/Controllers/PropertyController.cs
@@ -0,0 +1,40 @@
+﻿using aspclass2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspclass2.Controllers
+{
+    [Route("prop")]
+    public class PropertyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+       
+        [HttpGet]
+        public IActionResult Validation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Validation(Property p)
+        {
+            if(ModelState.IsValid)
+            {
+                return Content("You have successfully posted your property at FERA");
+            }
+            else
+            {
+                return Content("Something went wrong..Try posting the property again");
+            }
+        }
+
+            [Route("Fera")]
+        public String Home()
+        {
+            return "Welcome to Fera Luxe Property Management";
+        }
+    }
+}
---
+++ aspclass2/Models/Property.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace aspclass2.Models
+{
+    public class Property
+    {
+        [Key]
+        public Guid PropId { get; set; } = Guid.NewGuid();
+        [Required (ErrorMessage ="Property Name is required")]
+        public string PropName { get; set; }
+
+        
+        [StringLength(maximumLength:15,MinimumLength =6,ErrorMessage ="name should have 6 - 15 characters")]
+        public string OwnerName { get; set; }
+
+        [Range(0,15,ErrorMessage ="Property age should be 0 - 15 years old")]
+        public int PropAge { get; set; }
+
+        [EmailAddress(ErrorMessage ="Email id is invalid")]
+        public string Email { get; set; }
+
+       
+        public int Price { get; set; }
+
+        public DateTime PostedDate { get; set; }
+
+
+    }
+}
