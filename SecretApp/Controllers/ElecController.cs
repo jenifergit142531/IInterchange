2987b1177fc987538de707f1143503e49b8afca6
jenifery <jenifers@revature.com>
2023-11-09 01:39:09 -0500

    2

--- SecretApp/Controllers/ElecController.cs
+++ SecretApp/Controllers/ElecController.cs
@@ -1,12 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using SecretApp.Models;
 
 namespace SecretApp.Controllers
 {
     public class ElecController : Controller
     {
+        public IConfiguration Configuration { get; }
+
+        public ElecController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+       
+
         public IActionResult Index()
         {
-            return View();
+            List<ElecInventory> inventoryList = new List<ElecInventory>();
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string selectQuery = "select * from ElecInventory";
+                SqlCommand cmd = new SqlCommand(selectQuery, con);
+                using (SqlDataReader sdr = cmd.ExecuteReader())
+                {
+                    while(sdr.Read())
+                    {
+                        ElecInventory inventory = new ElecInventory();
+                        inventory.Id = Convert.ToInt32(sdr["id"]);
+                        inventory.Pname = Convert.ToString(sdr["pname"]);
+                        inventory.Price = Convert.ToInt32(sdr["price"]);
+                        inventory.Quantity = Convert.ToInt32(sdr["quantity"]);
+                        inventory.Updateddate = Convert.ToDateTime(sdr["updateddate"]);
+                        inventoryList.Add(inventory);
+
+                    }
+                    con.Close();
+                }
+            }
+
+                return View(inventoryList);
         }
     }
 }
