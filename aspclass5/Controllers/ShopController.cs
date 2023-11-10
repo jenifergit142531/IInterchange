d75a05decc7d82783f8f70f1e96e7e2c6b35903e
jenifery <jenifers@revature.com>
2023-11-09 23:51:35 -0500

    first

---
+++ aspclass5/Controllers/ShopController.cs
@@ -0,0 +1,46 @@
+﻿using aspclass5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspclass5.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["welcome"] = "Welcome to the property botique shop";
+            ViewBag.greetings = "Welcome all..Get 20% on your first service";
+            return View();
+        }
+        public IActionResult Purchase()
+        {
+            List<Shopping> ls = new List<Shopping>()
+            { 
+                new Shopping()
+            {
+                RegNo=1001,
+                ShopName="Miramax",
+                Location="chennai",
+                CustomerRating=5
+            },
+                 new Shopping()
+            {
+                RegNo=1002,
+                ShopName="Fera",
+                Location="ECR",
+                CustomerRating=4
+            },
+                  new Shopping()
+            {
+                RegNo=1003,
+                ShopName="Jgroup",
+                Location="Vellore",
+                CustomerRating=3
+            }
+            };
+            // ViewBag.ShopDetails = ls;
+            ViewData["ShopDetails"] = ls;
+           
+            return View();
+        }
+    }
+}
---
+++ aspclass5/Models/Shopping.cs
@@ -0,0 +1,11 @@
+﻿namespace aspclass5.Models
+{
+    public class Shopping
+    {
+        public int RegNo { get; set; }
+        public string ShopName { get; set; }
+        public string Location { get; set; }
+        public int CustomerRating { get; set; }
+
+    }
+}
