189bf64f6dc4979e4fe2e87f329c2d0938da810f
jenifery <jenifers@revature.com>
2024-01-25 02:37:37 -0500

    2

---
+++ Controllers/LocalizationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace GlobalMVC.Controllers
+{
+    public class LocalizationController : Controller
+    {
+        private readonly IStringLocalizer<LocalizationController> _localizer;
+
+        public LocalizationController(IStringLocalizer<LocalizationController> localizer)
+        {
+            _localizer = localizer;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Hello"] = _localizer["Hello"];
+            return View();
+        }
+    }
+}
