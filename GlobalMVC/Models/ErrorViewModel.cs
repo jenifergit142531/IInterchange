8f3b96f7a9ccf0241055a2c702a3a9202086a73b
jenifergit142531 <76467762+jenifergit142531@users.noreply.github.com>
2024-01-25 02:35:12 -0500

    Delete GlobalMVC directory

--- GlobalMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GlobalMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
