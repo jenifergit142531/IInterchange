b399e0eaf280f52be877b55c742568c6f28bb799
jenifery <jenifers@revature.com>
2024-02-01 02:31:44 -0500

    3

---
+++ Demos/angularclass2/src/app/directive/directive.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directive',
+  templateUrl: './directive.component.html',
+  styleUrl: './directive.component.css'
+})
+export class DirectiveComponent {
+
+  PropShop:any[]=[
+    {
+    Pid:'Prop001',PropName:'Avisa',Price:25000,Agent:'Aron',
+    Availability:'yes',LastUpdatedDate:'2/1/2024'
+     },
+     {
+      Pid:'Prop002',PropName:'Laguna',Price:35000,Agent:'Amy',
+      Availability:'no',LastUpdatedDate:'31/1/2024'
+       },
+       {
+        Pid:'Prop003',PropName:'Macarena',Price:15000,Agent:'steve',
+        Availability:'yes',LastUpdatedDate:'30/1/2024'
+         },
+         {
+          Pid:'Prop004',PropName:'Ivory',Price:30000,Agent:'Tom',
+          Availability:'no',LastUpdatedDate:'15/1/2024'
+           },
+     
+];
+
+price!:number;
+
+CheckPropertyPrice():boolean{
+  
+  return this.price>5000;
+}
+}
